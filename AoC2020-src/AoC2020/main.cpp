5554906bea8d6677a0d121a94457de4094430862
SimonD <simondwucet@gmx.de>
2020-12-02 00:15:32 +0100

    Add build environment

---
+++ AoC2020-src/AoC2020/main.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <QApplication>
+#include <QSettings>
+#include <QFileDialog>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Testing" << std::endl;
+}
---
+++ AoC2020-src/AoC2020/main.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include <vector>
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
